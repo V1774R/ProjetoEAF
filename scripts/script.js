e80367fc96c20fd1c6b2ea4f9ad80756b9093ee9
V1774R <157659240+V1774R@users.noreply.github.com>
2024-01-25 18:07:42 -0300

    Add files via upload
    Adicionando menu ao modo mobile

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+var statusMenu = false;
+function abreFechaMenu(){
+    //alert('Abrindo menu');
+     var menu = document.querySelector(".cabecalho__menus");
+    if(statusMenu == false){
+        //alert('abre');
+        
+        menu.style.height = "120px";
+        menu.style.top = "0";
+        menu.style.transition = "1s";
+        statusMenu = true;
+        
+    }else{
+        //alert('fecha');
+        menu.style.top = "-150px";
+        menu.style.height = "0";
+        menu.style.transition = "1s";
+        statusMenu = false;
+    }
+}
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.documentElement.scrollTop > 50) {
+        alert('rodou!')
+    } else {
+        alert('voltou');
+    }
+}
