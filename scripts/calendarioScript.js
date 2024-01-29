d4d2168046abe176502dc4dc065ac2686013d169
V1774R <157659240+V1774R@users.noreply.github.com>
2024-01-29 18:03:02 -0300

    Add files via upload
    datas e horarios disponiveis sendo geradas com JS;
    novas funçoes JS adicionadas

--- scripts/calendarioScript.js
+++ scripts/calendarioScript.js
@@ -4,10 +4,12 @@ var datas = document.querySelector(".datas");
 var btnNext = document.querySelector(".btnNext");
 var btnBack = document.querySelector(".btnBack");
 // MESES E DIAS DA SEMANA
-var dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
-var meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-var liberados = ["Fevereiro", 4, 6, 8, 10, 24, 25, 26];
+const dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+var liberados = [2024, "fevereiro", 4, 6, 8, 10, 24, 25, 26]; //indices 0 e 1 são reservados para o ano e o mes, respectivamente.
+var horariosDisponiveis = ["08:00", "09:00", "10:00", "11:00", "14:00", "15:00", "16:00", "17:00"];
 var classe;
+var exibirDetalhes = false;
 //Criando cabeçalho do calendário.
 var data = new Date();
 //alert(dataDeInicio);
@@ -17,6 +19,7 @@ var diaFinal = new Date(data.getFullYear(), data.getMonth()+1, 0).getDay();
 var totalDias = new Date(data.getFullYear(), data.getMonth()+1, 0).getDate();
 gerar();
 
+var mesSelecionado; //Variavel alterada pelas funções passar e voltar.
 function voltar(){
     data.setMonth(data.getMonth()-1);
     diaInicio = new Date(data.getFullYear(), data.getMonth(), 1).getDay();
@@ -44,12 +47,12 @@ function gerar(){
         datas.innerHTML += `<div class="data inativo">${inativosPre}<div>`;
     }
     var m = meses[data.getMonth()].toLowerCase();//pega o mes do sistema
-    var m2 = liberados[0].toLowerCase(); //confere o mes liberado pelo admin
+    var m2 = liberados[1].toLowerCase(); //confere o mes liberado pelo admin
 
     for(let i = 1; i<=totalDias; i++){
-        if(liberados.includes(i) && m == m2){
+        if(liberados.includes(i) && m == m2 && liberados[0] == data.getFullYear()){
             classe = "data dinamico verde";
-            datas.innerHTML += `<a href="#area" onclick="alert('Calma, ainda vou liberar de verdade kkk')"><div class="${classe}">${i}<div></a>`;
+            datas.innerHTML += `<a href="#area" onclick="abrirDetalhes(${i})"><div class="${classe}">${i} <div></a>`;
         }else{
             classe = "data";
             datas.innerHTML += `<div class="${classe}">${i}<div>`;
@@ -58,4 +61,39 @@ function gerar(){
     for(let i = 1; i <= 6 - diaFinal; i++){
         datas.innerHTML += `<div class="data inativo">${i}<div></a>`;
     }
+}
+
+
+
+function abrirDetalhes(x){
+    //alert("mostrando resultados para o dia:" + x);
+    var dia = new Date(data.getFullYear(), data.getMonth(), x);
+    var texto = document.querySelector(".calendario__detalhesDia__hora__data");
+    texto.innerHTML = `${dia.getDate()} de ${meses[dia.getMonth()]} de ${dia.getFullYear()}`;
+    if(exibirDetalhes == false){
+        var detalhes = document.querySelector(".calendario__detalhesDia");
+        detalhes.style.opacity = "1";
+        detalhes.style.transition = "2s";
+        detalhes.style.zIndex = "1";
+        exibirDetalhes = true;
+        mostrarHorarios();
+    }else{
+        var detalhes = document.querySelector(".calendario__detalhesDia");
+        detalhes.style.opacity = "0";
+        detalhes.style.transition = "2s";
+        detalhes.style.zIndex = "-1";
+        exibirDetalhes = false;
+    }
+
+}
+function mostrarHorarios(){
+    var elemento = document.querySelector(".local_exibir_horarios");
+    elemento.innerHTML = ``;
+    for(c=0; c < horariosDisponiveis.length; c++){
+        elemento.innerHTML += `<div>${horariosDisponiveis[c]} <a href="#" class="selec" onclick="confirmar()"> | selecionar</a></div>`;
+    }
+}
+function confirmar(){
+    alert('Ainda não é possível selecionar um horário. Está função está em construção e sera implementada em breve.');
+
 }
