3fb25116145856c68640edecfd8e28350921793f
V1774R <157659240+V1774R@users.noreply.github.com>
2024-01-31 15:44:26 -0300

    Add files via upload

--- scripts/calendario.js
+++ scripts/calendario.js
@@ -130,4 +130,5 @@ function resumir(){
     elemento.innerHTML += `<div> Dia: ${dia} de ${meses[mes]} </div>`;
     elemento.innerHTML += `<div> Horário: ${horariosDisponiveis[hora]} horas </div>`;
     elemento.innerHTML += `<div class="btn" style="cursor: pointer;"><a href="sucesso.html#sucesso" style="text-decoration: none; color: white;">Confirmar</a></div><br>`;
+    //php/confirmacao.php?nome=${nome}&cpf=${cpf}&telefone=${telefone}&dia=${dia}&mes=${mes}&hora=${hora}
 }
